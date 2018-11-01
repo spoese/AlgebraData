23ea331a9557eff7c06a3857864f2231d537244a
spoese <scott.poese@gmail.com>
2018-11-01 16:53:15 -0400

    Beta 0.2
    Added ability to filter by 045/050 or all.

--- app.R
+++ app.R
@@ -2,29 +2,35 @@ library(shiny)
 library(tidyverse)
 
 ui <- fluidPage(
-   
+        
         headerPanel("Displaying IKC vs. Midterm Grades"),
         sidebarPanel(
                 radioButtons("type", "Type of class:",
-                            choices = c("045","050","All"),
-                            selected = "All",
-                            inline = TRUE),
+                             choices = c("45","50","All"),
+                             selected = "All",
+                             inline = TRUE),
                 checkboxGroupInput("data","Which variables would you like to look at?",
-                              choices = c("Test Scores","IKC Scores","Time Spent", "Topics/Hour"),
-                              selected = c("Test Scores","IKC Scores"),
-                              inline = TRUE)
+                                   choices = c("Test Scores","IKC Scores","Time Spent", "Topics/Hour"),
+                                   selected = c("Test Scores","IKC Scores"),
+                                   inline = TRUE)
         ),
         mainPanel(
                 plotOutput("firstPlot")
         )
 )
 
-dat <- read_csv("FormattedData.csv")
+dat <- as.data.frame(read_csv("FormattedData.csv"))
 
 server <- function(input, output) {
+        myDat <- reactive({
+                if (input$type == "All"){
+                        dat
+                } else {
+                        filter(dat,Type == input$type)
+                }
+        })
         output$firstPlot <- renderPlot({
-                
-                g <- ggplot(dat,aes(x=IKC.Percent,y=Midterm.Percent)) +
+                g <- ggplot(myDat(),aes(x=IKC.Percent,y=Midterm.Percent)) +
                         geom_point() +
                         geom_smooth(method="lm")
                 g

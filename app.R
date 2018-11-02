36e2754e98584f78c6a5a27ff2940a002a639774
spoese <scott.poese@gmail.com>
2018-11-02 08:26:53 -0400

    Beta 0.3
    -Added ability to filter data by where the class meets, when the class started, and how many days/week the class meets.
    -Fixed plot scale to be static for easier comparisons between filters.
    -Added error message if no students match chosen filters.

--- app.R
+++ app.R
@@ -9,12 +9,22 @@ ui <- fluidPage(
                              choices = c("45","50","All"),
                              selected = "All",
                              inline = TRUE),
-                checkboxGroupInput("data","Which variables would you like to look at?",
-                                   choices = c("Test Scores","IKC Scores","Time Spent", "Topics/Hour"),
-                                   selected = c("Test Scores","IKC Scores"),
+                radioButtons("where", "Where does the class meet?",
+                             choices = c("DL","On-Campus","Anywhere"),
+                             selected = "Anywhere",
+                             inline = TRUE),
+                radioButtons("when", "When did the class start?",
+                             choices = c("On-time","Late","Any"),
+                             selected = "Any",
+                             inline = TRUE),
+                checkboxGroupInput("days","How many days/week does the class meet?",
+                                   choices = c("0","1","2","3","4"),
+                                   selected = c("0","1","2","3","4"),
                                    inline = TRUE)
         ),
         mainPanel(
+                h2(textOutput("error")),
+                tags$head(tags$style("#error{color: red")),
                 plotOutput("firstPlot")
         )
 )
@@ -22,17 +32,45 @@ ui <- fluidPage(
 dat <- as.data.frame(read_csv("FormattedData.csv"))
 
 server <- function(input, output) {
+        xlims <- reactive({
+                range(dat$IKC.Percent,na.rm = TRUE)
+        })
+        ylims <- reactive({
+                range(dat$Midterm.Percent, na.rm = TRUE)
+        })
         myDat <- reactive({
                 if (input$type == "All"){
-                        dat
+                        temp <- dat
                 } else {
-                        filter(dat,Type == input$type)
+                        temp <- filter(dat,Type == input$type)
                 }
+                if (input$where == "Anywhere"){
+                        temp <- temp
+                } else {
+                        temp <- filter(temp,Where == input$where)
+                }
+                if (input$when == "Any"){
+                        temp <- temp
+                } else {
+                        temp <- filter(temp,When == input$when)
+                }
+                temp <- filter(temp,Days %in% input$days)
+                temp
+        })
+        output$error <- renderText({
+                if (dim(myDat())[1] == 0){
+                        error <- "Warning: No students fit the filters you chose."
+                } else {
+                        error <- ""
+                }
+                error
         })
         output$firstPlot <- renderPlot({
                 g <- ggplot(myDat(),aes(x=IKC.Percent,y=Midterm.Percent)) +
                         geom_point() +
-                        geom_smooth(method="lm")
+                        geom_smooth(method = "lm") +
+                        xlim(xlims()) +
+                        ylim(ylims())
                 g
         })
 }
